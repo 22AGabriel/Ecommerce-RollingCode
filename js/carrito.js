696332d686dc6bb5ea17594800369ada2ee2eb1e
Javier Uncos <javiersrfc30@gmail.com>
2022-09-12 19:51:14 -0300

    agregamos un lista a la ventana modal del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -19,11 +19,31 @@ function agregarCarrito(codigoCarrito){
 
       carrito.forEach((itemCarrito)=>{
           tablaCarrito.innerHTML += `
-           <img src="${itemCarrito.imagen}" alt="" class="w-100">
-          <h5>${itemCarrito.nombre}</h5>
-          <p>${itemCarrito.precio}</p>
-          <button class="btn btn-success">Cancelar</button>
-          </    
-          `
+          <tr>
+          <td scope="row"><img class="w-100" src="${itemCarrito.imagen}" alt="${itemCarrito.nombre}"></td>
+          <td>${itemCarrito.codigo}</td>
+          <td>${itemCarrito.nombre}</td>
+          <td>${itemCarrito.precio}</td>
+          <td>${itemCarrito.descripcion}</td>
+          <td>${itemCarrito.categoria}</td>
+          <td>${itemCarrito.cantidad}</td>
+          <td>
+              <button type="button" class="btn btn-outline-danger" onclick="borrarProducto('${itemCarrito.codigo}')">
+                  <i class="bi bi-trash-fill"></i>
+              </button>
+              <button type="button" class="btn btn-outline-warning mt-2" onclick="editarProducto('${(itemCarrito.codigo)}')">
+                  <i class="bi bi-pencil-square"></i>
+              </button>
+          </td>
+       </tr>`
+          
       })
   }
+
+
+
+//   <img src="${itemCarrito.imagen}" alt="" class="w-100">
+//   <h5>${itemCarrito.nombre}</h5>
+//   <p>${itemCarrito.precio}</p>
+//   <button class="btn btn-success">Cancelar</button>
+//   </    
