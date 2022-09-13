b7a8d2376626041d19d51cb9623fbd2ee1095f39
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-09-13 17:01:16 -0300

    agregué validaciones para iniciar sesion solo con usuarios ya registrados

--- js/logIn.js
+++ js/logIn.js
@@ -1,8 +1,21 @@
+import {buscarUsuario, buscarContrasenia} from "./helper.js";
+
 const modalLogIn = new bootstrap.Modal(document.getElementById("modalInicioSesionIndex"));
 const btnLogIn = document.getElementById("btnLogIn");
+let usuarioLogIn = document.getElementById("usuario");
+let contraseniaLogIn = document.getElementById("contrasenia");
+let btnIniciar = document.getElementById("btnIniciar")
 
 btnLogIn.addEventListener("click", mostrarLogIn);
+btnIniciar.addEventListener("submit", logIn);
 
 function mostrarLogIn(){
-    modalLogIn.show(); 
+    modalLogIn.show();
+}
+
+function logIn(e){
+    e.preventDefault();
+    if(buscarUsuario(usuarioLogIn) && buscarContrasenia(usuarioLogIn, contraseniaLogIn)){
+        console.log("hola")
+    }
 }
