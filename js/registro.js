16ee5dbd0ca55e28d96e6cd5c69961750ad6f912
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-09-12 22:26:43 -0300

    se crea y guarda en el localStorage el arreglo de nuevos usuarios y se limpia el formulario

--- js/registro.js
+++ js/registro.js
@@ -1,16 +1,18 @@
 import { Usuario } from "./classUsuario.js";
 import { validarNombreUsuario, validarEmail, validarContraseniaRegistrada, validarContraseniaConfirmar} from "./helper.js"
 
-let listaUsuarios = []
+let listaUsuarios = JSON.parse(localStorage.getItem('listaUsuariosStorage')) || [];
 
 const modalRegistro = new bootstrap.Modal(document.getElementById("modalRegistrarse"));
 const btnRegistrarse = document.getElementById("btnRegistrarse");
+let formularioRegistro = document.getElementById("btnRegistrar")
 let nombreUsuario = document.getElementById("nombreUsuario")
 let email = document.getElementById("email")
 let contraseniaRegistro = document.getElementById("contraseniaRegistro")
 let contraseniaConfirmar = document.getElementById("contraseniaConfirmar")
 
 btnRegistrarse.addEventListener("click", showForm);
+formularioRegistro.addEventListener('submit', registrarse);
 nombreUsuario.addEventListener("blur", () => {validarNombreUsuario(nombreUsuario)});
 email.addEventListener("blur", () => {validarEmail(email)});
 contraseniaRegistro.addEventListener("blur", () => {validarContraseniaRegistrada(contraseniaRegistro)}); 
@@ -18,4 +20,30 @@ contraseniaConfirmar.addEventListener("blur", () => {validarContraseniaConfirmar
 
 function showForm(){
     modalRegistro.show()
+}
+
+function registrarse(e){
+    e.preventDefault();
+    let nuevoUsuario = new Usuario(
+        nombreUsuario.value,
+        email.value,
+        contraseniaRegistro.value
+    )
+    listaUsuarios.push(nuevoUsuario)
+    console.log(listaUsuarios)
+    guardarUsuarioEnLocalStorage();
+    limpiarFormulario();
+    modalRegistro.hide();
+}
+
+function guardarUsuarioEnLocalStorage(){
+    localStorage.setItem('listaUsuariosStorage', JSON.stringify(listaUsuarios))
+}
+
+function limpiarFormulario(){
+    formularioRegistro.reset();
+    nombreUsuario.className = "form-control bg-dark text-light";
+    email.className = "form-control bg-dark text-light";
+    contraseniaRegistro.className = "form-control bg-dark text-light";
+    contraseniaConfirmar.className = "form-control bg-dark text-light";
 }
